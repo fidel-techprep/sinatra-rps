a63ebc046cbdb3b0b3379888cfbc8d189c10984a
Fidel Leal <127764786+fidel-techprep@users.noreply.github.com>
2023-07-18 04:20:40 +0000

    refactored code

--- app.rb
+++ app.rb
@@ -1,47 +1,19 @@
 require "sinatra"
 require "sinatra/reloader"
 
-options = ["rock", "paper", "scissors"]
-
 get("/") do
   erb(:rules)
 end
 
-get("/rock") do
-  @we = "rock"
-  @they = options.sample
+get("/:choice") do
+  @lose_to = { "rock" => "paper", "paper" => "scissors", "scissors" => "rock" } 
+  @we = params.fetch("choice")
+  @they = @lose_to.keys.sample
   @result = "tied"
-
-  case @they
-  when "scissors" then @result = "won"
-  when "paper" then @result = "lost"
+  
+  unless @we == @they
+   @result =  @lose_to[@we] == @they ? "lost" : "won"
   end
-
-  erb(:play)
-end
-
-get("/paper") do
-  @we = "paper"
-  @they = options.sample
-  @result = "tied"
-
-  case @they
-  when "scissors" then @result = "lost"
-  when "rock" then @result = "won"
-  end
-
-  erb(:play)
-end
-
-get("/scissors") do
-  @we = "scissors"
-  @they = options.sample
-  @result = "tied"
-
-  case @they
-  when "rock" then @result = "lost"
-  when "paper" then @result = "won"
-  end
-
+  
   erb(:play)
 end
