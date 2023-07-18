0f7dd7d32ce0e9109fe701980ce4446dbca08602
Fidel Leal <127764786+fidel-techprep@users.noreply.github.com>
2023-07-18 03:08:04 +0000

    add rps match

--- app.rb
+++ app.rb
@@ -1,9 +1,47 @@
 require "sinatra"
 require "sinatra/reloader"
 
+options = ["rock", "paper", "scissors"]
+
 get("/") do
-  "
-  <h1>Welcome to your Sinatra App!</h1>
-  <p>Define some routes in app.rb</p>
-  "
+  erb(:rules)
+end
+
+get("/rock") do
+  @we = "rock"
+  @they = options.sample
+  @result = "tied"
+
+  case @they
+  when "scissors" then @result = "won"
+  when "paper" then @result = "lost"
+  end
+
+  erb(:play)
+end
+
+get("/paper") do
+  @we = "paper"
+  @they = options.sample
+  @result = "tied"
+
+  case @they
+  when "scissors" then @result = "lost"
+  when "rock" then @result = "won"
+  end
+
+  erb(:play)
+end
+
+get("/scissors") do
+  @we = "scissors"
+  @they = options.sample
+  @result = "tied"
+
+  case @they
+  when "rock" then @result = "lost"
+  when "paper" then @result = "won"
+  end
+
+  erb(:play)
 end
